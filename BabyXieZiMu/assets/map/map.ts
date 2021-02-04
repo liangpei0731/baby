158448902aecad9a255bc839d63f642d16b0d9a3
hualinge6 <274805430@qq.com>
2021-02-04 13:40:51 +0800

    程序测试版

--- BabyXieZiMu/assets/map/map.ts
+++ BabyXieZiMu/assets/map/map.ts
@@ -18,8 +18,8 @@ export default class map extends cc.Component {
     paths: Array<Array<any>> =  [];
     tempPath:cc.Vec2[] = [];
 
-    //是否正在获取路径点
-    isAutoGetPaths: boolean = false;
+    //当前状态0初始化、1自动绘制、2玩家描摹
+    currentState: number = 0;
     //当前动画索引
     pathsIndex: number = 0;
     //当前动画坐标索引
@@ -44,37 +44,34 @@ export default class map extends cc.Component {
         this.userMiaoMoGraphics = userMiaoMoGraphics;
     }
 
-    start () {
-
+    start () 
+    {
         //循环播放所有的路径动画
         var animation = this.arrow.getComponent(cc.Animation);
         var clips = animation.getClips();
         animation.on('play', function(type,state)
         {
             cc.log(type,state);
-            this.isAutoGetPaths = true;
+            this.currentState = 1;
             this.pathsPositionIndex = 0;
             this.tempPath = [];
         }, this);
         animation.on('finished', function(type,state)
         {
             this.paths.push(this.tempPath);
-            this.isAutoGetPaths = false;
+            this.currentState = 0;
             this.pathsPositionIndex = 0;
             this.tempPath = [];
             cc.log(type,state);
             if(this.pathsIndex + 1 < clips.length)
             {
                 this.pathsIndex++;
-                var pathsIndex = this.pathsIndex;
-                var clip = clips[pathsIndex];
-                //animation.play(clips[this.pathsIndex].name);
                 animation.node.runAction(cc.sequence(cc.delayTime(0.2),cc.callFunc(function(){
-                    animation.play(clip.name);
-                })));
+                    animation.play(clips[this.pathsIndex].name);
+                },this)));
             }else{
                 cc.log(this.paths);
-                this.isAutoGetPaths = false;
+                this.currentState = 0;
                 this.pathsIndex = 0;
                 this.pathsPositionIndex = 0;
 
@@ -84,16 +81,16 @@ export default class map extends cc.Component {
         }, this);
         if(this.pathsIndex < clips.length)
         {
+            //this.currentState = 1;
             animation.play(clips[this.pathsIndex].name);
         }
-
     }
 
     update (dt) 
     {
-        if(this.isAutoGetPaths)
+        if(this.currentState == 1)
         {
-            //获取自动绘画路径上的所有坐标点
+            //获取自动绘制路径以及保存路径上的所有坐标点
             var position: cc.Vec2 = this.arrow.node.getPosition();
             if(this.pathsPositionIndex == 0)
             {
@@ -109,53 +106,96 @@ export default class map extends cc.Component {
         }
     }
 
+    //初始化箭头的坐标和方向
+    initArrow()
+    {
+        if(this.paths.length < 0) return; 
+        if(this.paths[this.pathsIndex].length < 0) return;
+
+        this.pathsPositionIndex = 0;
+        var position = this.paths[this.pathsIndex][0];
+        var nextPosition = position;
+        if(this.paths[this.pathsIndex].length > 1)
+        {
+            nextPosition = this.paths[this.pathsIndex][1];
+        } 
+        
+        var  angle: number  = Math.atan2((position.y-nextPosition.y), (nextPosition.x-position.x))
+        var  theta: number  = angle*( 180 /Math.PI);
+        
+        this.arrow.node.position = position;
+        this.arrow.node.rotation = theta;
+
+        this.userMiaoMoGraphics.moveTo(position.x,position.y);
+    }
+
     initUserDraw()
     {
-        this.arrow.node.position = this.paths[this.pathsIndex][this.pathsPositionIndex];
+        //开始临摹
+        this.currentState = 2;
 
+        this.initArrow();
+        
         //添加玩家绘制事件
         this.arrow.node.on(cc.Node.EventType.TOUCH_START, (event) => 
         {
-            if(this.isAutoGetPaths)
-            {
-                return false;
-            }
-            var position: cc.Vec2 = this.arrow.node.getPosition();
-            this.userMiaoMoGraphics.moveTo(position.x,position.y);
+            if(this.currentState != 2) return false;
             this.touchBegin = true
         }, this);
 
         this.arrow.node.on(cc.Node.EventType.TOUCH_MOVE, (event) => 
         {
-            if (!this.touchBegin) {
-                return true;
-            }
+            if (!this.touchBegin) return true;
+            if(this.paths.length < 0) return true; 
+            if(this.paths[this.pathsIndex].length < 0) return true;
+            if(this.paths[this.pathsIndex].length <= this.pathsPositionIndex + 1) return true;
+            
             var touchPos = this.arrow.node.parent.convertToNodeSpaceAR(event.touch.getLocation());
-
-            //移动位置
-            // if(!this.arrow.node.parent.getBoundingBox().contains(touchPos))
-            // {
-            //     //this.touchBegin = false;
-            //     return true;
-            // }
+            var position = this.paths[this.pathsIndex][this.pathsPositionIndex+1];
+            
+            //超出临摹范围
+            var distance = position.sub(touchPos).mag();
+            if(distance > 30) return true;
+            
+            this.pathsPositionIndex++;
             var originalPos = this.arrow.node.getPosition();
-            this.arrow.node.position = touchPos;
+            this.arrow.node.position = position;
 
             //旋转角度
-            var p1 = touchPos;
+            var p1 = position;
             var p2 = originalPos;
-            var  angle: number  = Math.atan2((p2.y-p1.y), (p1.x-p2.x))  //弧度  0.6435011087932844
-            var  theta: number  = angle*( 180 /Math.PI);  //角度  36.86989764584402
-            //var  angle: number  = Math.atan2( (p1.y-p2.y) , (p2.x-p1.x))  //弧度 -0.6435011087932844, 即 2*Math.PI - 0.6435011087932844
-            //var  theta: number  = angle*( 180 /Math.PI);   //角度 -36.86989764584402，即360 - 36.86989764584402 = 323.13010235415598
-            //var  angle: number  = Math.atan2((p2.x - p1.x), (p2.y - p1.y))  //弧度  0.9272952180016122
-            //var  theta: number  = angle*( 180 /Math.PI);  //角度  53.13010235415598
+            var  angle: number  = Math.atan2((p2.y-p1.y), (p1.x-p2.x));
+            var  theta: number  = angle*( 180 /Math.PI);
             this.arrow.node.rotation = theta;
 
             //同步绘画层
-            this.userMiaoMoGraphics.lineTo(touchPos.x,touchPos.y);
+            this.userMiaoMoGraphics.lineTo(position.x,position.y);
             this.userMiaoMoGraphics.stroke();
-            //this.userMiaoMoGraphics.getComponent("userMiaoMoGraphics").lineTo(this.node.parent.convertToWorldSpaceAR(touchPos));
+
+            cc.log("当前路径点：",this.pathsPositionIndex+1,"/",this.paths[this.pathsIndex].length)
+            if(this.paths[this.pathsIndex].length > this.pathsPositionIndex + 1)
+            {
+               //继续下一个路径点
+            }else if(this.paths.length > this.pathsIndex + 1 && this.paths[this.pathsIndex].length == this.pathsPositionIndex + 1)
+            {
+                //最后一个坐标点跳入下一笔画
+                this.currentState = 2;
+                this.pathsIndex++;
+                this.pathsPositionIndex = 0;
+                this.initArrow();
+            }else{
+                //全部绘制完毕
+                this.node.runAction(cc.sequence(
+                    cc.delayTime(0.5)
+                    ,cc.callFunc(function(){
+                        this.currentState = 2;
+                        this.pathsIndex = 0;
+                        this.pathsPositionIndex = 0;
+                        this.userMiaoMoGraphics.clear();
+                        this.initArrow();
+                    },this)
+                ))
+            } 
         }, this);
 
         this.arrow.node.on(cc.Node.EventType.TOUCH_END, (event) => {
